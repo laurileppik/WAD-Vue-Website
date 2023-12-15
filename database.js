a8f5dd2f2ea3731c4aa51552d6399fb496bcc98a
lauri.leppik <laurile02@gmail.com>
2023-12-15 14:04:51 +0200

    create databases automatically

--- database.js
+++ database.js
@@ -7,4 +7,49 @@ const pool = new Pool({
     port: "5432"
 });
 
+
+const execute = async(query) => {
+    try {
+        await pool.connect(); // gets connection
+        await pool.query(query); // sends queries
+        return true;
+    } catch (error) {
+        console.error(error.stack);
+        return false;
+    }
+};
+
+const createPostsTable = `
+CREATE TABLE IF NOT EXISTS "posts" (
+    userid integer NOT NULL, 
+    createtime timestamp without time zone NOT NULL, 
+    id SERIAL PRIMARY KEY NOT NULL, 
+    likes integer DEFAULT 0, 
+    body text NOT NULL);
+    `;
+
+execute(createPostsTable).then(result => {
+    if (result) {
+        console.log('posts table created!');
+    }
+});
+
+
+const createUsersTable = `
+CREATE TABLE IF NOT EXISTS "users" (
+    id SERIAL PRIMARY KEY NOT NULL,
+    email character varying(255) NOT NULL,
+    password character varying(255) NOT NULL);
+    `;
+
+execute(createUsersTable).then(result => {
+    if (result) {
+        console.log('users table created!');
+    }
+});
+
+
+
+
+
 module.exports = pool;
